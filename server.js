9bd31c7853ee990e6dbe5f743bde0fa9262af07a
Adam Rogers <cldd5678@hotmail.com>
2018-07-06 17:51:00 -0400

    add dotenv

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 var express = require("express");
 var app = express();
+var dotEnv = require("dotenv").config();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -24,6 +25,6 @@ app.get("/api/hello", function(req, res) {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
+var listener = app.listen(process.env.PORT || 3000, function() {
 	console.log("Your app is listening on port " + listener.address().port);
 });
